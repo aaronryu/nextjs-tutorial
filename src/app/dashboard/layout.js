f86e05595ba5d5d3434e1ce7aff00e572aa7e86b
Aaron Ryu <fbcndah@gmail.com>
2024-05-27 18:30:06 +0900

    실습 1-7. 컴포넌트 기반 SPA Routing : /dashboard 의 Left Navigation Bar

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,15 +1,21 @@
-function LinkButton({ children }) {
-  return <div className='border-2 border-solid p-[10px] border-white block'>{children}</div>
+import Link from 'next/link'
+
+function LinkButton({ href, children }) {
+  return (
+    <Link href={href} className='border-2 border-solid p-[10px] border-white block'>
+      {children}
+    </Link>
+  )
 }
 
 export default function DashboardLayout({ children }) {
   return (
     <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
       <div className='w-full flex-none md:w-64'>
-        <LinkButton>Logo</LinkButton>
-        <LinkButton>1. Dashboard</LinkButton>
-        <LinkButton>2. Invoices</LinkButton>
-        <LinkButton>3. Customers</LinkButton>
+        <LinkButton href='/'>Logo</LinkButton>
+        <LinkButton href='/dashboard'>1. Dashboard</LinkButton>
+        <LinkButton href='/dashboard/invoices'>2. Invoices</LinkButton>
+        <LinkButton href='/dashboard/customers'>3. Customers</LinkButton>
       </div>
       <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>
         <div className='w-full h-[100px] bg-red-500' />
