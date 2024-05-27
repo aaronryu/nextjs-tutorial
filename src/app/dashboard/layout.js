d911d34ee119e569dc4f5f7bfca701ac1efa01b7
Aaron Ryu <fbcndah@gmail.com>
2024-05-27 18:27:06 +0900

    실습 1-6. /dashboard 의 Layout 을 두 영역으로 나눠 왼쪽에 Left Navigation Bar

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,8 +1,20 @@
+function LinkButton({ children }) {
+  return <div className='border-2 border-solid p-[10px] border-white block'>{children}</div>
+}
+
 export default function DashboardLayout({ children }) {
   return (
-    <>
-      <div className="w-full h-[100px] bg-red-500" />
-      {children}
-    </>
-  );
+    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
+      <div className='w-full flex-none md:w-64'>
+        <LinkButton>Logo</LinkButton>
+        <LinkButton>1. Dashboard</LinkButton>
+        <LinkButton>2. Invoices</LinkButton>
+        <LinkButton>3. Customers</LinkButton>
+      </div>
+      <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>
+        <div className='w-full h-[100px] bg-red-500' />
+        {children}
+      </div>
+    </div>
+  )
 }
