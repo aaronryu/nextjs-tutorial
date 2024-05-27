476937eeb4f5c851940bcfaa8efea68e14e0642d
Aaron Ryu <fbcndah@gmail.com>
2024-05-27 20:13:18 +0900

    실습 1-10. (B) Layout 내 Metadata 때문에 "use client" 사용을 위해 새 컴포넌트 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
-'use client'
-
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { useRouter } from 'next/navigation'
+import ButtonGroup from '@/app/button-group'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,27 +10,16 @@ export const metadata: Metadata = {
   description: '애런의 앞으로 고생길이 훤하다',
 }
 
-function Button({ children, onClick }) {
-  return (
-    <div className='border-2 border-solid p-[10px] border-white inline-block' onClick={onClick}>
-      {children}
-    </div>
-  )
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const router = useRouter()
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className='w-full h-[100px] bg-red-300 flex-row'>
-          <Button onClick={() => router.back()}>{'← 뒤로가기'}</Button>
-          <Button onClick={() => router.refresh()}>{'새로고침'}</Button>
-          <Button onClick={() => router.forward()}>{'앞으로가기 →'}</Button>
+        <div className='w-full h-[100px] bg-red-300'>
+          <ButtonGroup />
         </div>
         {children}
       </body>
