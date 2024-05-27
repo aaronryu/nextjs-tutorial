5c42639c367894b29b5332cea3312e22c9b29e57
Aaron Ryu <fbcndah@gmail.com>
2024-05-27 16:54:05 +0900

    실습 1-2. 새 페이지 만들기 : /dashboard 페이지를 만들기 위해 /dashboard 디렉토리 생성

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="text-red-500">Hello, World!</div>
+    </main>
+  );
+}
