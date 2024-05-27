9c2fda45108189c43baaab75552644707269ec72
Aaron Ryu <fbcndah@gmail.com>
2024-05-27 16:40:10 +0900

    실습 1-1. Layout : layout.js 내 <meta> 수정 및 연한 핑크 div 영역 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
+      <Image
+        src="/vercel.svg"
+        alt="Next.js 에서는 이미지를 사용할때 최적화가 적용된 자체 <Image/> 를 사용한다."
+        width={100}
+        height={24}
+        priority
+      />
       Hello, World!
     </main>
   );
