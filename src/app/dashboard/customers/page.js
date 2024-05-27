660423a9eb2cee2414e31b612641e7b07bad82c2
Aaron Ryu <fbcndah@gmail.com>
2024-05-27 17:17:34 +0900

    실습 1-3. 새 페이지 만들기 : /dashboard/customers 와 /dashboard/invoices 디렉토리 및 Page 생성

---
+++ src/app/dashboard/customers/page.js
@@ -0,0 +1,9 @@
+export default function Customers() {
+  return (
+    <main className='flex min-h-screen flex-col items-center p-24'>
+      <div className='text-gray-500'>
+        이 페이지는 /dashboard 의 Layout 을 계승받은 /dashboard/customers 페이지 입니다
+      </div>
+    </main>
+  )
+}
