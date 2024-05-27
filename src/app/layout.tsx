4335a2f380584336e97a88cde7a248dc4c04c09d
Aaron Ryu <fbcndah@gmail.com>
2024-05-27 19:24:31 +0900

    실습 1-10. (A) 주의 : Layout 에 Metadata 사용시엔 "use client" 사용불가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,41 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+'use client'
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { useRouter } from 'next/navigation'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "애런의 새로운 Next 프로젝트",
-  description: "애런의 앞으로 고생길이 훤하다",
-};
+  title: '애런의 새로운 Next 프로젝트',
+  description: '애런의 앞으로 고생길이 훤하다',
+}
+
+function Button({ children, onClick }) {
+  return (
+    <div className='border-2 border-solid p-[10px] border-white inline-block' onClick={onClick}>
+      {children}
+    </div>
+  )
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
+  const router = useRouter()
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <div className="w-full h-[100px] bg-red-300" />
+        <div className='w-full h-[100px] bg-red-300 flex-row'>
+          <Button onClick={() => router.back()}>{'← 뒤로가기'}</Button>
+          <Button onClick={() => router.refresh()}>{'새로고침'}</Button>
+          <Button onClick={() => router.forward()}>{'앞으로가기 →'}</Button>
+        </div>
         {children}
       </body>
     </html>
-  );
+  )
 }
