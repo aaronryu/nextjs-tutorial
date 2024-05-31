25bdfd62d6fe1c98d80742ae1a2e988988192d8d
Aaron Ryu <fbcndah@gmail.com>
2024-06-01 03:05:00 +0900

    실습 1-15. (group) 디렉토리를 통해 URL 가 생기진않으나 layout 으로 디자인 그룹핑 가능

---
+++ src/app/dashboard/(section-green)/layout.js
@@ -0,0 +1,8 @@
+export default function SectionGreenLayout({ children }) {
+  return (
+    <>
+      <div className='w-full h-[100px] bg-green-300'>(green) 그룹핑</div>
+      {children}
+    </>
+  )
+}
---
+++ src/app/dashboard/(section-lime)/layout.js
@@ -0,0 +1,8 @@
+export default function SectionLimeLayout({ children }) {
+  return (
+    <>
+      <div className='w-full h-[100px] bg-green-100'>(lime) 그룹핑</div>
+      {children}
+    </>
+  )
+}
