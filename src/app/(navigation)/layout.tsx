e08ae0420b670bbb6eebf6ba5779300b939f60cb
Aaron Ryu <fbcndah@gmail.com>
2025-03-01 21:40:36 +0900

    실습 9-1: Left Navigation Bar(LNB) 추가 - <a> anchor 태그 활용

--- src/app/(navigation)/layout.tsx
+++ src/app/(navigation)/layout.tsx
@@ -1,14 +1,33 @@
+function LinkButton({ href, children }) {
+  return (
+    <a
+      href={href}
+      className='border-2 border-solid p-[10px] border-white block'
+    >
+      {children}
+    </a>
+  )
+}
+
 export default function NavigationLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <>
-      <div className='w-full bg-red-700'>
-        모든 (navigation) 페이지에서의 공통영역 RootLayout
+    <div className='flex flex-row'>
+      <div className='w-full md:w-44'>
+        <LinkButton href='/'>Logo</LinkButton>
+        <LinkButton href='/dashboard'>1. Dashboard</LinkButton>
+        <LinkButton href='/dashboard/invoices'>2. Invoices</LinkButton>
+        <LinkButton href='/dashboard/customers'>3. Customers</LinkButton>
       </div>
-      {children}
-    </>
+      <div className='flex-grow'>
+        <div className='w-full bg-red-700'>
+          모든 (navigation) 페이지에서의 공통영역 RootLayout
+        </div>
+        {children}
+      </div>
+    </div>
   )
 }
