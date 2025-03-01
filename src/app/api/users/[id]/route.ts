9adc6b043d381a4ddd2aa668dc2bd025d1013f5a
Aaron Ryu <fbcndah@gmail.com>
2025-03-02 03:32:05 +0900

    실습 17-1: Query Parameter - /?key=name > Route Handler(REST API) 에 적용 시 NextRequest 에서 추출

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -6,3 +6,19 @@ export async function GET(request: NextRequest, { params }) {
   const found = users.filter((each) => String(each.id) === id)
   return NextResponse.json(found.length > 0 ? found[0] : null)
 }
+
+export async function PATCH(request: NextRequest, { params }) {
+  const { id } = await params
+  const searchParams = request.nextUrl.searchParams
+  const name = searchParams.get('name')
+  const job = searchParams.get('job')
+  const specialty = searchParams.get('specialty')
+
+  const found = users.filter((each) => String(each.id) === id)
+  const user = found.length > 0 ? found[0] : null
+  if (name) user.name = name
+  if (job) user.job = job
+  if (specialty) user.specialty = specialty
+
+  return NextResponse.json(users)
+}
