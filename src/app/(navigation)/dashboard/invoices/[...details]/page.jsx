732241db69235ec8a9927532ba3cb2d47cefbfa6
Aaron Ryu <fbcndah@gmail.com>
2025-03-02 02:21:10 +0900

    실습 15-1: Catch-all Segment - [...slug] 다수 세그먼트(다수 PathVariable) 한번에 처리 가능

---
+++ src/app/(navigation)/dashboard/invoices/[...details]/page.jsx
@@ -0,0 +1,16 @@
+export default async function InvoiceDetails({ params, searchParams }) {
+  console.log(await params)
+  console.log(await searchParams)
+
+  const { details } = await params
+  const type = details[0]
+  const desc = details[1]
+
+  return (
+    <main className='text-center bg-blue-50 p-6'>
+      어떤 요구사항인지 상세 내용을 볼까요?
+      <div>- 타입은 {type},</div>
+      <div>- 상세내용은 {desc}</div>
+    </main>
+  )
+}
