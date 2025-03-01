c210473e0a97340569731ab2ec96b1a5e4dd6ce2
Aaron Ryu <fbcndah@gmail.com>
2025-03-02 02:15:04 +0900

    실습 13-2: Single Segment - [slug] 단일 세그먼트 인입하기 위한 버튼 추가

--- src/app/(navigation)/dashboard/customers/page.jsx
+++ src/app/(navigation)/dashboard/customers/page.jsx
@@ -1,3 +1,5 @@
+import ServerLinkButton from '@/components/ServerLinkButton'
+
 const wait = (time) => new Promise((resolve) => setTimeout(resolve, time))
 
 export default async function Customer() {
@@ -6,6 +8,9 @@ export default async function Customer() {
   return (
     <main className='text-center bg-blue-100 p-6 border-b-8 border-blue-700'>
       <div className='text-blue-500'>Customer 페이지</div>
+      <ServerLinkButton href='customers/aaron'>Aaron 고객</ServerLinkButton>
+      <ServerLinkButton href='customers/baron'>Baron 고객</ServerLinkButton>
+      <ServerLinkButton href='customers/caron'>Caron 고객</ServerLinkButton>
     </main>
   )
 }
