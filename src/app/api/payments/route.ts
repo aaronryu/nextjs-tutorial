3d3c5c51e33ece251dda04fa8c0f3e777b868b66
Aaron Ryu <fbcndah@gmail.com>
2025-03-03 00:38:23 +0900

    실습 3-3: Route Handler - POST /api/payments + { category: 'small', ... }

--- src/app/api/payments/route.ts
+++ src/app/api/payments/route.ts
@@ -1,6 +1,13 @@
-import { payments } from '@/app/lib/placeholder-data'
+import { generatePaymentId, payments } from '@/app/lib/placeholder-data'
 import { NextResponse } from 'next/server'
 
 export async function GET(request, context) {
+  return NextResponse.json(payments)
+}
+
+export async function POST(request, context) {
+  const requestBody = await request.json()
+  requestBody.id = generatePaymentId()
+  payments.push(requestBody)
   return NextResponse.json(payments)
 }
