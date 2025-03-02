499d9fbeedc9708fd4a1a9dafdedd20fa1b7fa59
Aaron Ryu <fbcndah@gmail.com>
2025-03-02 15:27:14 +0900

    실습 1-2: (복습) Route Handler - POST /api/users + await request.json() 역직렬화

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -4,3 +4,10 @@ import { NextResponse } from 'next/server'
 export async function GET(request, context) {
   return NextResponse.json(users)
 }
+
+export async function POST(request, context) {
+  const requestBody = await request.json()
+  requestBody.id = generateUserId()
+  users.push(requestBody)
+  return NextResponse.json(users)
+}
