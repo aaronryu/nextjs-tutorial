1d69967b5143cc8d189da599a2446a3431de64db
Aaron Ryu <fbcndah@gmail.com>
2025-03-03 18:28:43 +0900

    실습 3-4: Route Handler - PUT /api/payments + (form-data) ?key=value

--- src/app/api/payments/[id]/route.ts
+++ src/app/api/payments/[id]/route.ts
@@ -34,3 +34,31 @@ export async function PATCH(request: NextRequest, { params }) {
 
   return NextResponse.json(payments)
 }
+
+export async function PUT(request: NextRequest, { params }) {
+  const { id } = await params
+  const formData = await request.formData()
+  const category: string = formData.get('category') as string
+  const product = formData.get('product') as string
+  const quantity = formData.get('quantity')
+  const unitPrice = formData.get('unitPrice')
+  const userId = formData.get('userId')
+
+  const found = payments.filter((each) => String(each.id) === id)
+  const payment = found.length > 0 ? found[0] : null
+  if (category) payment.category = category
+  if (product) payment.product = product
+  if (quantity) {
+    payment.quantity = isNaN(Number(quantity)) ? 1 : Number(quantity)
+    payment.totalPrice = payment.unitPrice * payment.quantity
+  }
+  if (unitPrice) {
+    payment.unitPrice = isNaN(Number(unitPrice)) ? 0 : Number(unitPrice)
+    payment.totalPrice = payment.unitPrice * payment.quantity
+  }
+  if (userId) {
+    payment.userId = isNaN(Number(userId)) ? undefined : Number(userId)
+  }
+
+  return NextResponse.json(payments)
+}
